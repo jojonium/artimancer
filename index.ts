bb60a1484909ab00db22ce55fbe247d839fbfc6d
Joseph Petitti <jppetitti@wpi.edu>
2020-01-21 11:35:15 -0500

    move to using ts-node

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-const express = require("express");
-const path = require("path");
+import * as express from "express";
+import { resolve } from "path";
 const app = express();
 
 // get port and hostname from environment variables, or use defaults if not set
@@ -7,13 +7,13 @@ const PORT = +process.env.NODE_PORT || 3000;
 const HOSTNAME = process.env.NODE_HOSTNAME || "localhost";
 
 // directory to serve static content from
-app.use(express.static(path.resolve(__dirname, "static")));
+app.use(express.static(resolve(__dirname, "static")));
 
 // serve images from the images directory
-app.use("/images", express.static(path.resolve(__dirname, "images")));
+app.use("/images", express.static(resolve(__dirname, "images")));
 
 // serve fonts from the fonts directory
-app.use("/fonts", express.static(path.resolve(__dirname, "fonts")));
+app.use("/fonts", express.static(resolve(__dirname, "fonts")));
 
 // start the server
 app.listen(PORT, HOSTNAME, () => {
